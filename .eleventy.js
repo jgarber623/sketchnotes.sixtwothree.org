6fc1b7065a94bd039e93130b9550f18b72ddc5a5
Jason Garber <jason@sixtwothree.org>
2022-01-29 22:07:21 -0500

    Use eleventyConfig per convention

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,17 @@
-module.exports = eleventy => {
-  eleventy.setBrowserSyncConfig(require('@jgarber/browsersync-config/eleventy'));
+module.exports = eleventyConfig => {
+  eleventyConfig.setBrowserSyncConfig(require('@jgarber/browsersync-config/eleventy'));
 
-  eleventy.addCollection('posts', collection => {
+  eleventyConfig.addCollection('posts', collection => {
     return collection.getFilteredByGlob('./src/_posts/*.md').reverse();
   });
 
-  eleventy.addFilter('prettify', require('./lib/filters/prettify.js'));
+  eleventyConfig.addFilter('prettify', require('./lib/filters/prettify.js'));
 
-  eleventy.addPassthroughCopy('./src/favicon.ico');
-  eleventy.addPassthroughCopy('./src/jgarber.png');
-  eleventy.addPassthroughCopy('./src/robots.txt');
+  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('./src/jgarber.png');
+  eleventyConfig.addPassthroughCopy('./src/robots.txt');
 
-  eleventy.addWatchTarget('./src/assets/stylesheets');
+  eleventyConfig.addWatchTarget('./src/assets/stylesheets');
 
   return {
     dir: {

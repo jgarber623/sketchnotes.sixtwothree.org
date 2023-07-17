b3aad8f092059efbe05ecc6a4f425b3a70324065
Jason Garber <jason@sixtwothree.org>
2023-07-17 09:59:44 -0400

    Simplify Eleventy config, update .gitignore

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,9 +17,7 @@ module.exports = function(eleventyConfig) {
 
   return {
     dir: {
-      input: './src',
-      layouts: '_layouts',
-      output: './public'
+      input: './src'
     }
   };
 };
